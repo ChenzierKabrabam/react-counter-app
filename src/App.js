8ba684c685e6faea23bd012f189678be353d72f3
Chenzier Kabrabam <74149266+ChenzierKabrabam@users.noreply.github.com>
2021-03-09 15:41:17 +0530

    fixed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Card from '@material-ui/core/Card'
+import CardContent from '@material-ui/core/CardContent'
+import CardActions from '@material-ui/core/CardActions'
+import Fab from '@material-ui/core/Fab'
+import * as AiIcons from 'react-icons/ai'
+import { makeStyles } from '@material-ui/core/styles'
+
+const styles = makeStyles({
+  root: {
+    width: '50%',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: '100px auto',
+  },
+  text: {
+    fontSize: 38,
+    fontWeight: 'bolder',
+  },
+})
 
 function App() {
+  const classes = styles()
+  let [count, setCount] = React.useState(0)
+
+  const formatCount = () => {
+    return count === 0 ? 'Zero' : count
+  }
+
+  const handleIncrement = () => {
+    setCount(count + 1)
+  }
+
+  const handleDecrement = () => {
+    if (count !== 0) {
+      setCount(count - 1)
+    }
+  }
+
+  const handleReset = () => {
+    if (count !== 0) {
+      setCount((count = 0))
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Card className={classes.root}>
+      <CardContent>
+        <h1 className={classes.text}>{formatCount()}</h1>
+      </CardContent>
+
+      <CardActions>
+        <Fab color='primary' aria-label='add' onClick={handleDecrement}>
+          <AiIcons.AiOutlineMinus />
+        </Fab>
+
+        <Fab color='primary' aria-label='edit' onClick={handleIncrement}>
+          <AiIcons.AiOutlinePlus />
+        </Fab>
+
+        <Fab
+          variant='extended'
+          aria-label='clear'
+          color='secondary'
+          onClick={handleReset}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <h2>Reset</h2>
+        </Fab>
+      </CardActions>
+    </Card>
+  )
 }
 
-export default App;
+export default App
